e35264639ea1516ed3c98ec7075676101923db08
Chris Yip <chrisyipw@gmail.com>
2013-03-12 03:25:37 +0800

    major fixes

--- source/js/theme.js
+++ source/js/theme.js
@@ -1,18 +1,18 @@
-(function($) {
-  if (!$) return;
+( function ( $ ) {
+  if ( !$ ) return;
   $( document )
-  .on( 'click', '.toggle-menu', function(e) {
+  .on( 'click', '.toggle-menu', function ( e ) {
     e.stopPropagation()
     $( this ).closest( '.header-menu' ).toggleClass( 'on' )
   })
-  .on( 'click', '.search', function(e) {
+  .on( 'click', '.search', function ( e ) {
     e.stopPropagation()
     $( this ).toggleClass( 'on' ).find( '.search-field' ).focus()
   })
-  .on( 'click', '.search-field', function(e) {
+  .on( 'click', '.search-field', function ( e ) {
     e.stopPropagation()
   })
-  .on( 'click', function() {
+  .on( 'click', function () {
     $( '#header .on' ).removeClass( 'on' )
   })
-})(jQuery);
+} )( window.jQuery || window.Zepto );
