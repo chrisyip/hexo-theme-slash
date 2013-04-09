9a19df2de4d93deca750ea8a7e1e220dc63c95e9
Chris Yip <chrisyipw@gmail.com>
2013-04-09 15:28:21 +0800

    faster script

--- source/js/theme.js
+++ source/js/theme.js
@@ -1,18 +1,35 @@
-( function ( $ ) {
-  if ( !$ ) return;
-  $( document )
-  .on( 'click', '.toggle-menu', function ( e ) {
-    e.stopPropagation()
-    $( this ).closest( '.header-menu' ).toggleClass( 'on' )
-  })
-  .on( 'click', '.search', function ( e ) {
-    e.stopPropagation()
-    $( this ).toggleClass( 'on' ).find( '.search-field' ).focus()
-  })
-  .on( 'click', '.search-field', function ( e ) {
-    e.stopPropagation()
-  })
-  .on( 'click', function () {
-    $( '#header .on' ).removeClass( 'on' )
-  })
-} )( window.jQuery || window.Zepto );
+( function(){
+  var matches, toggle_class
+
+  matches = (function(){
+    var m = document.body.webkitMatchesSelector || document.body.mozMatchesSelector || document.body.oMatchesSelector || document.body.msMatchesSelector || document.body.matchesSelector
+    return function ( elem, sel ) {
+      m.call( elem, sel )
+    };
+  })()
+
+  toggle_class = function ( elem, klass ) {
+    elem.classList.contains( klass ) ? elem.classList.remove( klass ) : elem.classList.add( klass )
+  }
+
+  document.addEventListener( 'click', function ( e ) {
+    var elem = e.target
+
+    if ( matches( elem, '.toggle-menu' ) ) {
+      ( function(){
+        if ( matches( this, '.header-menu' ) ) {
+          toggle_class( this, 'on' )
+        } else {
+          arguments.callee.call( null, this.parentElement )
+        }
+      } ).call( elem.parentElement )
+      return;
+    }
+
+    if ( matches( elem, '.search' ) ) {
+      toggle_class( elem, 'on' )
+      elem.querySelector( '.search-field' ).focus()
+      return;
+    }
+  }, false )
+} )();
